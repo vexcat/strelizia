a5eb70dbae880c339ec2babb3f8feb424d77f9e9
Gabriel Pulido <autodeskgato@gmail.com>
2019-09-14 11:32:25 -0700

    Upgrade OkapiLib

--- include/okapi/api.hpp
+++ include/okapi/api.hpp
@@ -1,4 +1,4 @@
-/**
+/*
  * @author Ryan Benasutti, WPI
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
@@ -7,15 +7,37 @@
  */
 #pragma once
 
+/** \mainpage OkapiLib Index Page
+ *
+ * \section intro_sec Introduction
+ *
+ * **OkapiLib** is a PROS library for programming VEX V5 robots. This library is intended to raise
+ * the floor for teams with all levels of experience. New teams should have an easier time getting
+ * their robot up and running, and veteran teams should find that OkapiLib doesn't get in the way or
+ * place any limits on functionality.
+ *
+ * For tutorials on how to get the most out of OkapiLib, see the **Tutorials** section:
+ *
+ * [Tutorials](docs/tutorials/index.md)
+ *
+ * For documentation on using the OkapiLib **API**, see the OkapiLib **API** section:
+ *
+ * <a href="annotated.html">OkapiLib Classes</a>
+ *
+ * Not sure where to go? Take a look at the `Getting Started tutorial
+ * <tutorials/walkthrough/getting-started.html>`_.
+ */
+
 #include "okapi/api/chassis/controller/chassisControllerIntegrated.hpp"
 #include "okapi/api/chassis/controller/chassisControllerPid.hpp"
 #include "okapi/api/chassis/controller/chassisScales.hpp"
+#include "okapi/api/chassis/controller/defaultOdomChassisController.hpp"
+#include "okapi/api/chassis/controller/odomChassisController.hpp"
 #include "okapi/api/chassis/model/readOnlyChassisModel.hpp"
 #include "okapi/api/chassis/model/skidSteerModel.hpp"
 #include "okapi/api/chassis/model/threeEncoderSkidSteerModel.hpp"
 #include "okapi/api/chassis/model/xDriveModel.hpp"
-#include "okapi/impl/chassis/controller/chassisControllerFactory.hpp"
-#include "okapi/impl/chassis/model/chassisModelFactory.hpp"
+#include "okapi/impl/chassis/controller/chassisControllerBuilder.hpp"
 
 #include "okapi/api/control/async/asyncLinearMotionProfileController.hpp"
 #include "okapi/api/control/async/asyncMotionProfileController.hpp"
@@ -33,11 +55,17 @@
 #include "okapi/api/control/util/flywheelSimulator.hpp"
 #include "okapi/api/control/util/pidTuner.hpp"
 #include "okapi/api/control/util/settledUtil.hpp"
-#include "okapi/impl/control/async/asyncControllerFactory.hpp"
+#include "okapi/impl/control/async/asyncMotionProfileControllerBuilder.hpp"
+#include "okapi/impl/control/async/asyncPosControllerBuilder.hpp"
+#include "okapi/impl/control/async/asyncVelControllerBuilder.hpp"
 #include "okapi/impl/control/iterative/iterativeControllerFactory.hpp"
 #include "okapi/impl/control/util/controllerRunnerFactory.hpp"
 #include "okapi/impl/control/util/pidTunerFactory.hpp"
 #include "okapi/impl/control/util/settledUtilFactory.hpp"
+
+#include "okapi/api/odometry/odomMath.hpp"
+#include "okapi/api/odometry/odometry.hpp"
+#include "okapi/api/odometry/threeEncoderOdometry.hpp"
 
 #include "okapi/api/device/rotarysensor/continuousRotarySensor.hpp"
 #include "okapi/api/device/rotarysensor/rotarySensor.hpp"

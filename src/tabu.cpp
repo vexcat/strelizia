2627b7b7f319296e5a29d7cac11c2f1b169bc51f
Gabriel Pulido <autodeskgato@gmail.com>
2019-11-14 21:20:42 -0800

    RAII & encoding-less tabu

--- src/tabu.cpp
+++ src/tabu.cpp
@@ -4,100 +4,6 @@
 
 using json = nlohmann::json;
 
-//Functions for encoding and decoding tabu's
-//variant of base64. It is almost the same as
-//normal URL-safe base64, but replacing 'p'
-//with '@' as it is the PROS command character.
-namespace encoding {
-
-  std::string encode(const std::string& data) {
-    static constexpr char sEncodingTable[] = {
-      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-      'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-      'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-      'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-      'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-      'o', '@', 'q', 'r', 's', 't', 'u', 'v',
-      'w', 'x', 'y', 'z', '0', '1', '2', '3',
-      '4', '5', '6', '7', '8', '9', '-', '_'
-    };
-
-    size_t in_len = data.size();
-    size_t out_len = 4 * ((in_len + 2) / 3);
-    std::string ret(out_len, '\0');
-    size_t i;
-    char *p = const_cast<char*>(ret.c_str());
-
-    for (i = 0; i < in_len - 2; i += 3) {
-      *p++ = sEncodingTable[(data[i] >> 2) & 0x3F];
-      *p++ = sEncodingTable[((data[i] & 0x3) << 4) | ((int) (data[i + 1] & 0xF0) >> 4)];
-      *p++ = sEncodingTable[((data[i + 1] & 0xF) << 2) | ((int) (data[i + 2] & 0xC0) >> 6)];
-      *p++ = sEncodingTable[data[i + 2] & 0x3F];
-    }
-    if (i < in_len) {
-      *p++ = sEncodingTable[(data[i] >> 2) & 0x3F];
-      if (i == (in_len - 1)) {
-        *p++ = sEncodingTable[((data[i] & 0x3) << 4)];
-        *p++ = '=';
-      }
-      else {
-        *p++ = sEncodingTable[((data[i] & 0x3) << 4) | ((int) (data[i + 1] & 0xF0) >> 4)];
-        *p++ = sEncodingTable[((data[i + 1] & 0xF) << 2)];
-      }
-      *p++ = '=';
-    }
-
-    return ret;
-  }
-
-  std::string decode(const std::string& input) {
-    std::string out;
-    static constexpr unsigned char kDecodingTable[] = {
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64,
-      52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
-      41,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-      15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 63,
-      64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-      64, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-      64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
-    };
-
-    size_t in_len = input.size();
-    if (in_len % 4 != 0) throw std::runtime_error("Input data size is not a multiple of 4");
-
-    size_t out_len = in_len / 4 * 3;
-    if (input[in_len - 1] == '=') out_len--;
-    if (input[in_len - 2] == '=') out_len--;
-
-    out.resize(out_len);
-
-    for (size_t i = 0, j = 0; i < in_len;) {
-      uint32_t a = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
-      uint32_t b = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
-      uint32_t c = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
-      uint32_t d = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
-
-      uint32_t triple = (a << 3 * 6) + (b << 2 * 6) + (c << 1 * 6) + (d << 0 * 6);
-
-      if (j < out_len) out[j++] = (triple >> 2 * 8) & 0xFF;
-      if (j < out_len) out[j++] = (triple >> 1 * 8) & 0xFF;
-      if (j < out_len) out[j++] = (triple >> 0 * 8) & 0xFF;
-    }
-
-    return out;
-  }
-
-}
-
 //Creates a random alphanumeric string of length len.
 std::string makeid(int len) {
   std::string ret;
@@ -129,7 +35,8 @@ Message::Message(const std::string& text) {
   if(addressEnd == text.size()) {
     throw std::runtime_error("No delimeter.");
   }
-  address = text.substr(1, addressEnd - 1);
+  //Really hacky way to parse out any \u0070's that pop up in the address
+  address = json::parse("\"" + text.substr(1, addressEnd - 1) + "\"").get<std::string>();
   addressEnd++;
   auto idEnd = text.find_first_of('/', addressEnd);
   auto idLen = idEnd - addressEnd;
@@ -192,6 +99,30 @@ void runLambdaAsync(T copyableRunnable) {
   }, copy, "runLambdaAsync");
 }
 
+class TabuLock {
+  bool taken = false;
+  public:
+  TabuLock() {
+    take();
+  }
+  ~TabuLock() {
+    if(taken) give();
+  }
+  void give() {
+    tabu_lock.give();
+    taken = false;
+  }
+  void take() {
+    bool wasTaken = tabu_lock.take(500);
+    if(!wasTaken) {
+      std::cerr << "We've got a problem." << std::endl;
+      char *badPtr = nullptr;
+      *badPtr = '#';
+    }
+    taken = true;
+  }
+};
+
 //Storage for topic listeners.
 std::vector<std::pair<std::string, std::function<void(const Message&)>>> topicListeners;
 //The provided function will be called when the given topic is received.
@@ -205,9 +136,9 @@ void tabu_on(const std::string& topic, std::function<void(const Message&)> liste
       });
     };
   }
-  tabu_lock.take(TIMEOUT_MAX);
-  topicListeners.push_back({topic, listener});
-  tabu_lock.give();
+  { TabuLock lk;
+    topicListeners.push_back({topic, listener});
+  }
 }
 
 using ReplyListener = std::pair<Message, std::function<void(const Message&, const Message&)>>;
@@ -226,16 +157,16 @@ void tabu_on(const Message& msg, std::function<void(const Message&, const Messag
       });
     };
   }
-  tabu_lock.take(TIMEOUT_MAX);
-  for(int i = 0; i < orphanReplies.size(); i++) {
-    if(orphanReplies[i].address == msg.id) {
-      listener(orphanReplies[i], msg);
-      orphanReplies.erase(orphanReplies.begin() + i);
-      return;
+  { TabuLock lk;
+    for(int i = 0; i < orphanReplies.size(); i++) {
+      if(orphanReplies[i].address == msg.id) {
+        listener(orphanReplies[i], msg);
+        orphanReplies.erase(orphanReplies.begin() + i);
+        return;
+      }
     }
+    replyListeners.push_back({msg, listener});
   }
-  replyListeners.push_back({msg, listener});
-  tabu_lock.give();
 }
 
 json helpRegistry = json::object();
@@ -300,10 +231,10 @@ void tabu_handler(const std::string& line) {
       tabu_handler_first_call = false;
       tabu_init();
     }
-    Message msg(encoding::decode(line));
+    Message msg(line);
     if(msg.addressKind == EVENT) {
       if(msg.address == "file-transfer") {
-        tabu_lock.take(TIMEOUT_MAX);
+        TabuLock lk;
         auto origID = msg.content["origID"].get<std::string>();
         if(ongoingTransfers.find(origID) == ongoingTransfers.end()) {
           ongoingTransfers[origID] = {
@@ -314,7 +245,7 @@ void tabu_handler(const std::string& line) {
         }
         auto& xfer = ongoingTransfers[origID];
         xfer["text"] = xfer["text"].get<std::string>() + msg.content["nextData"].get<std::string>();
-        tabu_lock.give();
+        lk.give();
         //Always send a reply
         tabu_send(msg);
         if(msg.content["done"].get<bool>()) {
@@ -323,20 +254,18 @@ void tabu_handler(const std::string& line) {
           constructed.address = xfer["address"].get<std::string>().substr(1);
           constructed.id = xfer["id"].get<std::string>();
           constructed.content = json::parse(xfer["text"].get<std::string>());
-          tabu_lock.take(TIMEOUT_MAX);
-          ongoingTransfers.erase(ongoingTransfers.find(origID));
-          tabu_lock.give();
-          tabu_handler(encoding::encode(constructed.text()));
+          { TabuLock lk; ongoingTransfers.erase(ongoingTransfers.find(origID)); }
+          tabu_handler(constructed.text());
         }
       } else {
-        tabu_lock.take(TIMEOUT_MAX);
+        TabuLock lk;
         std::vector<decltype(topicListeners)::value_type> matching;
         for(auto& listener: topicListeners) {
           if(listener.first == msg.address) {
             matching.push_back(listener);
           }
         }
-        tabu_lock.give();
+        lk.give();
         for(auto& listener: matching) {
           try {
             listener.second(msg);
@@ -349,7 +278,7 @@ void tabu_handler(const std::string& line) {
       if(msg.addressKind == REPLY) {
         auto found = false;
         std::vector<ReplyListener> matching;
-        tabu_lock.take(TIMEOUT_MAX);
+        TabuLock lk;
         replyListeners.erase(std::remove_if(replyListeners.begin(), replyListeners.end(),
         [&](const ReplyListener& parent) {
             if(parent.first.id == msg.address) {
@@ -362,7 +291,7 @@ void tabu_handler(const std::string& line) {
         if(matching.empty()) {
           orphanReplies.push_back(msg);
         }
-        tabu_lock.give();
+        lk.give();
         for(auto& parent: matching) {
           try {
             parent.second(msg, parent.first);
@@ -382,7 +311,6 @@ void tabu_handler(const std::string& line) {
 //Sends a line of text to serial output, after encoding it.
 pros::Mutex tabu_lock;
 void tabu_say(const std::string& text) {
-  tabu_lock.take(TIMEOUT_MAX);
-  puts(encoding::encode(text).c_str());
-  tabu_lock.give();
+  TabuLock lk;
+  puts(text.c_str());
 }

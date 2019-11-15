8f45715e732b0816901d25ff7cd514d8c21bc152
Gabriel Pulido <autodeskgato@gmail.com>
2019-11-14 21:22:34 -0800

    Add tabu encoder-getters

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -1,8 +1,24 @@
 #include "main.h"
 #include "okapi/api.hpp"
 #include "tabu.hpp"
+#include "mtrs.hpp"
 
 //No longer any extra sensors on robot.
 void init_sensors() {
-
+  tabu_reply_on("enc_base", []() -> json {
+    return mtrs->all.getPosition();
+  });
+  tabu_help("enc_base", json::array({ treplyaction("say(it)") }));
+  tabu_reply_on("tilter_enc", []() -> json {
+    return mtrs->tilter.getPosition();
+  });
+  tabu_help("enc_tilter", json::array({ treplyaction("say(it)") }));
+  tabu_reply_on("enc_lift", []() -> json {
+    return mtrs->lift.getPosition();
+  });
+  tabu_help("enc_lift", json::array({ treplyaction("say(it)") }));
+  tabu_reply_on("enc_intake", []() -> json {
+    return mtrs->intake.getPosition();
+  });
+  tabu_help("enc_intake", json::array({ treplyaction("say(it)") }));
 }

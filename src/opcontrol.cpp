a1da0c8a11c9a21f1092765ce8d7ba30a9f71e0a
Gabriel Pulido <autodeskgato@gmail.com>
2019-11-14 21:22:08 -0800

    New controls

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -24,8 +24,6 @@ void resumeControl() {
 	while(!opcontrolActiveAck) pros::delay(1);
 }
 
-void autonomous();
-
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -47,21 +45,61 @@ void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	mtrs->intake.setBrakeMode(okapi::AbstractMotor::brakeMode::brake);
 	uint32_t liftActivatedAt = pros::millis();
+	mtrs->lift.setEncoderUnits(okapi::AbstractMotor::encoderUnits::rotations);
+	mtrs->tilter.setEncoderUnits(okapi::AbstractMotor::encoderUnits::rotations);
+	double initialTilterPos = mtrs->tilter.getPosition();
+	double initialLiftPos = mtrs->lift.getPosition();
+	mtrs->lift.setBrakeMode(okapi::AbstractMotor::brakeMode::brake);
+	mtrs->tilter.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+	int automaticTiltActivationTime = -1;
+	bool tilterInUse = false;
+	bool liftInUse = false;
 	while (true) {
 		if(!opcontrolActive) {
 			opcontrolActiveAck = false;
 			pros::delay(10);
 			continue;
 		}
+		opcontrolActiveAck = true;
 		double y_ctrl  = master.get_analog(ANALOG_LEFT_Y) / 127.0;
 		double x_ctrl = master.get_analog(ANALOG_LEFT_X) / 127.0;
-		mtrs->lift.controllerSet(-master.get_analog(ANALOG_RIGHT_Y) / 127.0);
+		double liftControl = -master.get_analog(ANALOG_RIGHT_Y) / 127.0;
+		if(!liftInUse || liftInUse) {
+			mtrs->lift.controllerSet(liftControl);
+			liftInUse = false;
+		}
+		
+		double tiltControl = master.get_digital(DIGITAL_R1) - master.get_digital(DIGITAL_L1);
+		if(!tilterInUse || tiltControl) {
+			mtrs->tilter.controllerSet(tiltControl);
+			tilterInUse = false;
+		}
+
+		//Arms down, Tray in
+		if(master.get_digital_new_press(DIGITAL_Y)) {
+			mtrs->lift.moveAbsolute(initialLiftPos, 200);
+			liftInUse = true;
+			mtrs->left.controllerSet(0);
+			mtrs->right.controllerSet(0);
+			mtrs->tilter.controllerSet(0);
+			mtrs->intake.controllerSet(0);
+			pros::delay(400);
+			mtrs->tilter.moveAbsolute(initialTilterPos, 200);
+			tilterInUse = true;
+		}
+		
+		//Tray in
+		if(master.get_digital_new_press(DIGITAL_X)) {
+			mtrs->tilter.moveAbsolute(initialTilterPos, 200);
+			tilterInUse = true;
+		}
 
 		mtrs->left .controllerSet(y_ctrl + x_ctrl);
 		mtrs->right.controllerSet(y_ctrl - x_ctrl);
 
-		mtrs->tilter.controllerSet(master.get_digital(DIGITAL_R1) - master.get_digital(DIGITAL_L1));
-		mtrs->intake.controllerSet(master.get_digital(DIGITAL_R2) - (master.get_digital(DIGITAL_L2) ? 0.75 : 0));
+		//If the arms are far away, intake slower.
+		double intakeMultiplier = int(std::abs(mtrs->lift.getPosition() - initialLiftPos) > 0.75) * 0.2;
+		mtrs->intake.controllerSet((master.get_digital(DIGITAL_R2) ? 1 : 0) - master.get_digital(DIGITAL_L2));
 
 		pros::delay(10);
 	}

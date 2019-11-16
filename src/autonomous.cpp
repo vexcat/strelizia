98ba7ba107de1bcada4387b0483a182865718b2c
Gabriel Pulido <autodeskgato@gmail.com>
2019-11-16 08:10:35 -0800

    Write autonomous

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -24,10 +24,10 @@ static void doPID(double max, double revs, int time, bool shouldTurn, okapi::Ite
   printf("%f fV, %f fE, %f acc on straight of %f\n", out.getActualVelocity(), ctrl.getError(), acc, revs);
 }
 
-static void straightPID(double max, double revs, int time) {
+static void straightNormal(double max, double revs, int time) {
   doPID(max, revs, time, false, okapi::IterativePosPIDController(
-    1.3,
-    0.015,
+    1.38,
+    0.017,
     0.009,
     0,
     okapi::TimeUtilFactory::create(),
@@ -36,21 +36,46 @@ static void straightPID(double max, double revs, int time) {
   mtrs->all.moveVoltage(0);
 }
 
-static void turnPID(double max, double revs, int time) {
+static void turnNormal(double max, double revs, int time) {
   //Create PID controller from parameters in msg.
   doPID(max, revs, time, true, okapi::IterativePosPIDController(
-    1.35,
-    0.014,
-    0.026,
+    1.3,
+    0.017,
+    0.029,
+    0,
+    okapi::TimeUtilFactory::create(),
+    std::make_unique<okapi::AverageFilter<3>>()
+  ), mtrs->turn);
+  mtrs->all.moveVoltage(0);
+}
+
+static void straightHeavy(double max, double revs, int time) {
+  doPID(max, revs, time, false, okapi::IterativePosPIDController(
+    1.38,
+    0.017,
+    0.008,
     0,
     okapi::TimeUtilFactory::create(),
     std::make_unique<okapi::AverageFilter<5>>()
+  ), mtrs->all);
+  mtrs->all.moveVoltage(0);
+}
+
+static void turnHeavy(double max, double revs, int time) {
+  //Create PID controller from parameters in msg.
+  doPID(max, revs, time, true, okapi::IterativePosPIDController(
+    1.2,
+    0.026,
+    0.029,
+    0,
+    okapi::TimeUtilFactory::create(),
+    std::make_unique<okapi::AverageFilter<3>>()
   ), mtrs->turn);
   mtrs->all.moveVoltage(0);
 }
 
 static bool amBlue = false;
-int which = 1;
+int which = 0;
 
 //Runs a callable and copyable type, T, as a pros::Task.
 //This really should be in its own file.
@@ -113,33 +138,49 @@ void whip() {
 
 void autonomous() {
   mtrs->all.setEncoderUnits(okapi::AbstractMotor::encoderUnits::rotations);
+  mtrs->tilter.setEncoderUnits(okapi::AbstractMotor::encoderUnits::rotations);
   auto startTime = pros::millis();
   if(which == 0) {
-    whip();
+    parallel::waitForAll({[&]{
+      /*
+      mtrs->all.controllerSet(-1);
+      pros::delay(300);
+      mtrs->all.controllerSet(0);
+      pros::delay(500);
+      */
+      straightNormal(0.8, 2.5, 1400);
+      straightNormal(1, -2, 1300);
+    }, [&] {
+      pros::delay(200);
+      mtrs->intake.controllerSet(1);
+      pros::delay(2000);
+      mtrs->intake.controllerSet(0);
+    }});
+    turnHeavy(0.6, 0.68, 950);
+    straightHeavy(1, 2, 1200);
+    turnHeavy(0.6, -0.67, 950);
     mtrs->intake.controllerSet(1);
-    straightPID(0.6, 1.8, 900);
-    for(int i = 0; i < 4; i ++) {
-      straightPID(0.6, (4 - 1.8) / 4, 800);
-    }
-  }
-  if(which == 1) {
-    straightPID(0.7, -2.4, 2000);
-    straightPID(0.7, 0.3, 2000);
-    // turnPID(0.7, -1.25, 2000);
-    // straightPID(0.7, 0.5, 2000);
-    // whip();
-  }
-  if(which == 2) {
-    whip();
-  }
-  if(which == 3) {
-    whip();
-    straightPID(0.6, 0.3, 2000);
-    turnPID(0.6, -1.3, 2000);
-    straightPID(0.6, -2, 3000);
+    straightHeavy(0.5, 2.9, 2000);
+    parallel::waitForAll({[&]{
+      straightHeavy(0.5, -1.6, 1400);
+    }, [&]{
+      pros::delay(800);
+      mtrs->intake.controllerSet(0);
+    }});
+    turnHeavy(0.5, 1.3, 700);
+    parallel::waitForAll({[&]{
+      straightHeavy(1, 1.7, 1000);
+      mtrs->tilter.moveRelative(3.98, 200);
+      pros::delay(3400);
+    }, [&]{
+      pros::delay(900);
+      mtrs->intake.moveVoltage(-4000);
+    }});
+    //YEET
+    straightHeavy(0.6, -1, 1000);
   }
   printf("Finished in %lums.\n", pros::millis() - startTime);
-  auto remainderTime = 15000 + startTime - pros::millis();
+  auto remainderTime = 15000 + (int)startTime - (int)pros::millis();
   if(remainderTime > 0) pros::delay(remainderTime);
   printf("Time's up!\n");
   mtrs->all.moveVoltage(0);
